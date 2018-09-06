f97efbefebfbf77433b4280e40edaffa6f4226e2
didier <didier.colaes@student.ap.be>
2018-09-06 16:48:43 +0200

    com2
    get + post

--- djangoApp/djangoApplication/views.py
+++ djangoApp/djangoApplication/views.py
@@ -2,7 +2,25 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.conf.settings import PROJECT_ROOT
 
 # Create your views here.
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
-def
+
+def index(request):
+    #Open the file back and read the contents
+    f=open("movies.txt", "r")
+    fl =f.readlines()
+    for x in fl:
+        movies.append(x)
+    return render(request,'templates/index.html',{'movies': movies})
+
+#rediscommands
+# set movies "the godfather"
+
+@cache_page(CACHE_TTL)
+def post(request, name):
+
+
+
